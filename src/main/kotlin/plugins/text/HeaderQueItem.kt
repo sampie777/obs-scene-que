4365578744c50746e7d0bd78bf84aaa7c3aefef2
S. Jansen <sa.jansen1@gmail.com>
2020-05-18 11:33:07 +0200

    Optimized code for textPlugin items

--- src/main/kotlin/plugins/text/HeaderQueItem.kt
+++ src/main/kotlin/plugins/text/HeaderQueItem.kt
@@ -1,69 +1,18 @@
 package plugins.text
 
-import GUI
-import handles.QueItemTransferHandler
 import objects.que.Que
 import plugins.common.QueItem
 import themes.Theme
-import java.awt.BorderLayout
 import java.awt.Color
 import java.awt.Font
-import java.awt.event.MouseAdapter
-import java.awt.event.MouseEvent
-import javax.swing.*
+import javax.swing.BorderFactory
+import javax.swing.JLabel
 import javax.swing.border.CompoundBorder
 import javax.swing.border.EmptyBorder
 
 class HeaderQueItem(override val plugin: TextPlugin, override val name: String) : QueItem {
 
-    companion object {
-        fun createPanelForQueItem(plugin: TextPlugin): JComponent {
-            val panel = JPanel(BorderLayout(10, 10))
-            panel.border = CompoundBorder(
-                CompoundBorder(
-                    EmptyBorder(5, 0, 5, 0),
-                    BorderFactory.createLineBorder(Color(180, 180, 180))
-                ),
-                EmptyBorder(10, 10, 10, 10)
-            )
-
-            val textField = JTextField()
-
-            val addButton = JButton("+")
-            addButton.toolTipText = "Click or drag to add"
-            addButton.addActionListener {
-                if (textField.text.isEmpty()) {
-                    return@addActionListener
-                }
-
-                val queItem = HeaderQueItem(plugin, textField.text)
-                textField.text = ""
-
-                Que.add(queItem)
-                GUI.refreshQueItems()
-            }
-            addButton.transferHandler = QueItemTransferHandler()
-            addButton.addMouseMotionListener(object : MouseAdapter() {
-                override fun mouseDragged(e: MouseEvent) {
-                    if (textField.text.isEmpty()) {
-                        return
-                    }
-
-                    val queItem = HeaderQueItem(plugin, textField.text)
-                    textField.text = ""
-
-                    val transferHandler = (e.source as JButton).transferHandler as QueItemTransferHandler
-                    transferHandler.queItem = queItem
-                    transferHandler.exportAsDrag(e.source as JComponent, e, TransferHandler.COPY)
-                }
-            })
-
-            panel.add(JLabel("Header"), BorderLayout.PAGE_START)
-            panel.add(textField, BorderLayout.CENTER)
-            panel.add(addButton, BorderLayout.LINE_END)
-            return panel
-        }
-    }
+    companion object : TextPluginCompanion(HeaderQueItem::class.java, "Header");
 
     override fun activate() {
         // Skip this que item by going to the next one
--- src/main/kotlin/plugins/text/PlainTextQueItem.kt
+++ src/main/kotlin/plugins/text/PlainTextQueItem.kt
@@ -1,67 +1,10 @@
 package plugins.text
 
-import GUI
-import handles.QueItemTransferHandler
-import objects.que.Que
 import plugins.common.QueItem
-import java.awt.BorderLayout
-import java.awt.Color
-import java.awt.event.MouseAdapter
-import java.awt.event.MouseEvent
-import javax.swing.*
-import javax.swing.border.CompoundBorder
-import javax.swing.border.EmptyBorder
 
 class PlainTextQueItem(override val plugin: TextPlugin, override val name: String) : QueItem {
 
-    companion object {
-        fun createPanelForQueItem(plugin: TextPlugin) : JComponent {
-            val panel = JPanel(BorderLayout(10, 10))
-            panel.border = CompoundBorder(
-                CompoundBorder(
-                    EmptyBorder(5, 0, 5, 0),
-                    BorderFactory.createLineBorder(Color(180, 180, 180))
-                ),
-                EmptyBorder(10, 10, 10, 10)
-            )
-
-            val textField = JTextField()
-
-            val addButton = JButton("+")
-            addButton.toolTipText = "Click or drag to add"
-            addButton.addActionListener {
-                if (textField.text.isEmpty()) {
-                    return@addActionListener
-                }
-
-                val queItem = PlainTextQueItem(plugin, textField.text)
-                textField.text = ""
-
-                Que.add(queItem)
-                GUI.refreshQueItems()
-            }
-            addButton.transferHandler = QueItemTransferHandler()
-            addButton.addMouseMotionListener(object : MouseAdapter() {
-                override fun mouseDragged(e: MouseEvent) {
-                    if (textField.text.isEmpty()) {
-                        return
-                    }
-
-                    val queItem = PlainTextQueItem(plugin, textField.text)
-                    textField.text = ""
-
-                    val transferHandler = (e.source as JButton).transferHandler as QueItemTransferHandler
-                    transferHandler.queItem = queItem
-                    transferHandler.exportAsDrag(e.source as JComponent, e, TransferHandler.COPY)
-                }
-            })
-
-            panel.add(JLabel("Plain text"), BorderLayout.PAGE_START)
-            panel.add(textField, BorderLayout.CENTER)
-            panel.add(addButton, BorderLayout.LINE_END)
-            return panel
-        }
-    }
+    companion object : TextPluginCompanion(PlainTextQueItem::class.java, "Plain text");
 
     override fun activate() {}
 
