b9fd15f0a77a5f897948f7beabd9f7a860852490
S. Jansen <sa.jansen1@gmail.com>
2020-05-21 14:14:39 +0200

    Added more visual feedback about Execute After Previous items

--- src/main/kotlin/gui/list/QueListCellRenderer.kt
+++ src/main/kotlin/gui/list/QueListCellRenderer.kt
@@ -3,12 +3,13 @@ package gui.list
 import brightness
 import plugins.common.QueItem
 import themes.Theme
-import java.awt.Component
+import java.awt.*
 import javax.swing.DefaultListCellRenderer
-import javax.swing.JLabel
 import javax.swing.JList
 
 class QueListCellRenderer : DefaultListCellRenderer() {
+
+    var item: QueItem? = null
 
     override fun getListCellRendererComponent(
         list: JList<*>,
@@ -17,13 +18,13 @@ class QueListCellRenderer : DefaultListCellRenderer() {
         isSelected: Boolean,
         cellHasFocus: Boolean
     ): Component {
-        val cell = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus) as JLabel
+        val cell = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus) as QueListCellRenderer
         if (value == null) {
             return cell
         }
 
-        val item = value as QueItem
-        item.getListCellRendererComponent(cell, index, isSelected, cellHasFocus)
+        cell.item = value as QueItem
+        cell.item!!.getListCellRendererComponent(cell, index, isSelected, cellHasFocus)
 
         if (brightness(cell.background) > 110) {
             cell.foreground = Theme.get.LIST_SELECTION_FONT_COLOR_DARK
@@ -32,5 +33,23 @@ class QueListCellRenderer : DefaultListCellRenderer() {
         }
 
         return cell
+    }
+
+    override fun paintComponent(g: Graphics) {
+        super.paintComponent(g)
+
+        if (item != null && item!!.executeAfterPrevious) {
+            paintExecuteAfterPrevious(g as Graphics2D)
+        }
+    }
+
+    private fun paintExecuteAfterPrevious(g2: Graphics2D) {
+        g2.color = Color(180, 180, 180)
+        g2.stroke = BasicStroke(1F)
+
+        val leftPadding = 16
+        val horizontalLineWidth = 5
+        g2.drawLine(leftPadding, 0, leftPadding, height / 2)
+        g2.drawLine(leftPadding, height / 2, leftPadding + horizontalLineWidth, height / 2)
     }
 }
